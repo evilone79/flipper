5bb709d6546ac921b210408fde56d2bce6080811
Mike D <michael@firebolt.io>
2021-03-22 23:48:55 +0200

    disable ssl for paho.cpp

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ init_build paho.mqtt.c
 $RUN_CMAKE -DPAHO_WITH_SSL=FALSE ../ && make -j4 && make install
 
 init_build $base_path/paho.mqtt.cpp
-$RUN_CMAKE -DPAHO_MQTT_C_LIBRARIES="$lib_path"/libpaho-mqtt3a.so -DPAHO_MQTT_C_INCLUDE_DIRS="$include_path" -DCMAKE_INSTALL_RPATH="$lib_path" ../ \
+$RUN_CMAKE -DPAHO_WITH_SSL=FALSE -DPAHO_MQTT_C_LIBRARIES="$lib_path"/libpaho-mqtt3a.so -DPAHO_MQTT_C_INCLUDE_DIRS="$include_path" -DCMAKE_INSTALL_RPATH="$lib_path" ../ \
 && make -j4 && make install
 
 init_build $base_path
